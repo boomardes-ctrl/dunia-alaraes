758bada1d742605834b72d996326d9aa0dd5356e
boomardes-ctrl <boomardes-ctrl@users.noreply.github.com>
2026-07-06 16:28:08 +0300

    تغيير صور المعرض من SVG إلى PNG + تبسيط المكون

--- server/db.js
+++ server/db.js
@@ -102,7 +102,7 @@ const defaultSettings = {
   email: 'info@dunyaalaræs.com',
   exchangeRate: '250',
   socialLinks: '[]',
-  gallery: '["/gallery/1.svg","/gallery/2.svg","/gallery/3.svg","/gallery/4.svg","/gallery/5.svg"]',
+  gallery: '["/gallery/1.png","/gallery/2.png","/gallery/3.png","/gallery/4.png","/gallery/5.png"]',
 };
 
 const insertSetting = db.prepare('INSERT OR IGNORE INTO settings (key, value) VALUES (?, ?)');
